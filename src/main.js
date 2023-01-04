2429d2bd049ed19c715a471352468622eae6a318
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-01-04 21:17:06 +0100

    refactor: reformat

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import './app.css'
+import "./app.css";
 import BubbleButton from "./lib/BubbleButton.svelte";
 
-if (process.env.NODE_ENV === 'development') {
-    new BubbleButton({
-        target: document.body,
-        props: {
-            botName: 'ViteBot',
-        },
-    });
+if (process.env.NODE_ENV === "development") {
+  new BubbleButton({
+    target: document.body,
+    props: {
+      botName: "ViteBot",
+    },
+  });
 } else {
-    window.ChatBot = BubbleButton;
+  window.ChatBot = BubbleButton;
 }
