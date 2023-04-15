dc71802a4a29cba3b9d49f5b6619b2627645fe5e
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-04-15 13:50:45 +0200

    feat: added sources

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import "./app.css";
 import BubbleButton from "./lib/BubbleButton.svelte";
 
-// if (process.env.NODE_ENV === "development") {
-//   new BubbleButton({
-//     target: document.body,
-//     props: {
-//       botName: "ICT4S Assistant",
-//       mainColor: "#337ab7"
-//     },
-//   });
-// } else {
-window.ChatBot = BubbleButton;
-// }
+new BubbleButton({
+  target: document.body,
+  props: {
+    botName: "ICT4S Assistant",
+    mainColor: "#337ab7",
+  },
+});
---
+++ src/main.prod.js
@@ -0,0 +1,5 @@
+import "./app.css";
+import BubbleButton from "./lib/BubbleButton.svelte";
+
+// @ts-ignore
+window.ChatBot = BubbleButton;
